5ca51f348544c72409caff121583dd5eaeb8d375
kerruish <bryan@kerruish.ie>
2018-01-25 20:53:50 +0000

    add organiser model

---
+++ CMS/Models/CMSModel/Organiser.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMS.Models.CMSModel
+{
+    public class Organiser
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrganiserId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string DisplayName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactDetails { get; set; }
+    }
+}
