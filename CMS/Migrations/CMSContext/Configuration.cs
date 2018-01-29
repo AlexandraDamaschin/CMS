1509bd2ad4083a41814cd7f45708e84a35b787e1
kerruish <bryan@kerruish.ie>
2018-01-29 14:10:24 +0000

    add location seeding

--- CMS/Migrations/CMSContext/Configuration.cs
+++ CMS/Migrations/CMSContext/Configuration.cs
@@ -4,6 +4,11 @@ namespace CMS.Migrations.CMSContext
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Models.CMSModel;
+    using System.Reflection;
+    using System.IO;
+    using System.Text;
+    using CsvHelper;
 
     internal sealed class Configuration : DbMigrationsConfiguration<CMS.Models.CMSModel.CmsContext>
     {
@@ -15,18 +20,37 @@ namespace CMS.Migrations.CMSContext
 
         protected override void Seed(CMS.Models.CMSModel.CmsContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            SeedLocations(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        //  Seed locations from embedded IrishTowns.csv
+        private void SeedLocations(CMS.Models.CMSModel.CmsContext context)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "CMS.Migrations.CMSContext.IrishTowns.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.HasHeaderRecord = true;
+                    var locationData = csvReader.GetRecords<LocationDataImport>().ToArray();
+
+                    foreach (var locationItem in locationData)
+                    {
+                        context.Locations.AddOrUpdate(m =>
+                                new { m.Name, m.Town, m.County, m.Lat, m.Lng },
+                            new Location
+                            {
+                                Name = locationItem.Town,
+                                Town = locationItem.Town,
+                                County = locationItem.County,
+                                Lat = locationItem.Latitude,
+                                Lng = locationItem.Longitude,
+                            });
+                    }
+                }
+            }
         }
     }
 }
---
+++ CMS/Models/CMSModel/LocationDataImport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Models.CMSModel
+{
+    public class LocationDataImport
+    {
+        public string Town { get; set; }
+        public string County { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public int Elevation { get; set; }
+        public int Population { get; set; }
+    }
+}
