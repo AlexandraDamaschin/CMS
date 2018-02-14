15f690168af5fe3cb06237760c762ed5ae6b4a13
kerruish <bryan@kerruish.ie>
2018-02-14 11:13:59 +0000

    add event tags viewmodel

---
+++ CMS/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CMS.Models.CMSModel;
+using System.Web.Mvc;
+
+namespace CMS.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public Event Event { get; set; }
+
+        public IEnumerable<SelectListItem> AllTags { get; set; }
+
+        private List<int> _associatedTags;
+        public List<int> AssociatedTags
+        {
+            get
+            {
+                if (_associatedTags == null)
+                {
+                    _associatedTags = Event.tags.Select(t => t.TagId).ToList();
+                }
+
+                return _associatedTags;
+            }
+            set { _associatedTags = value; }
+        }
+    }
+}
