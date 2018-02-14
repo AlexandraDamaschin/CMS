1c77b654d5cfa09e6b792878af7205201a367886
rubabramzan96 <rubabramzan96@gmail.com>
2018-02-14 12:14:27 +0000

    layout page sorted

--- CMS/App_Start/BundleConfig.cs
+++ CMS/App_Start/BundleConfig.cs
@@ -34,54 +34,21 @@ namespace CMS
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/datatables/css/dataTables.bootstrap.css",
-            //          "~/Content/themes/base/jquery-ui.css",
-            //          "~/Content/themes/base/datepicker.css",
-            //          "~/Content/jquery-ui-timepicker-addon.css",
-            //          "~/Content/toastr.css",
-            //          "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/bundles/css").Include(
+            bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/sbTheme/vendor/bootstrap/css/bootstrap.min.css",
                       "~/Content/sbTheme/vendor/metisMenu/metisMenu.min.css",
                       "~/Content/sbTheme/dist/css/sb-admin-2.css",
                       "~/Content/sbTheme/vendor/morrisjs/morris.css",
                       "~/Content/sbTheme/vendor/font-awesome/css/font-awesome.min.css",
 
-                               //  "~/Content/bootstrap.css",
-                                 "~/Content/datatables/css/dataTables.bootstrap.css",
-                                 "~/Content/themes/base/jquery-ui.css",
-                                 "~/Content/themes/base/datepicker.css",
-                                 "~/Content/jquery-ui-timepicker-addon.css",
-                                 "~/Content/toastr.css"
-                               //  "~/Content/site.css"
+                  //  "~/Content/bootstrap.css",
+                      "~/Content/datatables/css/dataTables.bootstrap.css",
+                      "~/Content/themes/base/jquery-ui.css",
+                      "~/Content/themes/base/datepicker.css",
+                      "~/Content/jquery-ui-timepicker-addon.css",
+                      "~/Content/toastr.css"
+                  //  "~/Content/site.css"
                        ));
-
-
-            //        bundles.Add(new ScriptBundle("~/bundles/lib").Include(
-
-            //"~/Content/sbTheme/vendor/jquery/jquery.min.js",
-            //                   "~/Content/sbTheme/vendor/bootstrap/js/bootstrap.min.js",
-            //                   "~/Content/sbTheme/vendor/metisMenu/metisMenu.min.js",
-            //                   "~/Content/sbTheme/vendor/raphael/raphael.min.js",
-            //                   "~/Content/sbTheme/vendor/morrisjs/morris.min.js",
-            //                   "~/Content/sbTheme/data/morris-data.js",
-            //                   "~/Content/sbTheme/dist/js/sb-admin-2.min.js",
-            //                    "~/Scripts/jquery-{version}.js",
-            //                    "~/Scripts/bootstrap.js",
-            //                    "~/Scripts/bootbox.js",
-            //                    "~/Scripts/respond.js",
-            //                    "~/Scripts/DataTables/jquery.dataTables.js",
-            //                    "~/Scripts/DataTables/datatables.bootstrap.js",
-            //                    "~/Scripts/jquery-ui-{version}.js",
-            //                    "~/Scripts/jquery-ui-timepicker-addon.js",
-            //                    "~/Scripts/toastr.js"));
-
-
-
-
 
         }
     }
