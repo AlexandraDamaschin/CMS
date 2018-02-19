1d7274a4c9e29514239bfc6fc30ecc7d1cf6a6aa
kerruish <bryan@kerruish.ie>
2018-02-19 15:22:26 +0000

    update event category dto

--- CMS/Dtos/EventCategoryDto.cs
+++ CMS/Dtos/EventCategoryDto.cs
@@ -11,12 +11,6 @@ namespace CMS.Dtos
 
         public string Name { get; set; }
 
-        public bool Outdoor { get; set; }
-
-        public bool Family { get; set; }
-
-        public bool Free { get; set; }
-
         public string Icon { get; set; }
     }
 }
