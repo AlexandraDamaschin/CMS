3e7dbfb97ae6d491cd8e4a9000c97e10786abb14
kerruish <bryan@kerruish.ie>
2018-02-19 12:02:27 +0000

    refactor locations and event category controllers

---
+++ CMS/ViewModels/EventCategoryFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CMS.Models.CMSModel;
+
+namespace CMS.ViewModels
+{
+    public class EventCategoryFormViewModel
+    {
+        public EventCategory EventCategory { get; set; }
+    }
+}
