2732b17299b53e9b9a00f3dce8a902d9c8ca8e1e
kerruish <bryan@kerruish.ie>
2018-02-24 13:11:23 +0000

    Add event category title

--- CMS/ViewModels/EventCategoryFormViewModel.cs
+++ CMS/ViewModels/EventCategoryFormViewModel.cs
@@ -6,20 +6,7 @@ namespace CMS.ViewModels
     {
         public EventCategory EventCategory { get; set; }
 
-//        public string Title => EventCategory.EventCategoryId != 0 ? "Edit Event Category" : "New Event Category";
-//
-//        public EventCategoryFormViewModel()
-//        {
-//            EventCategory.EventCategoryId = 0;
-//        }
-//
-//        public EventCategoryFormViewModel(EventCategory eventCategory)
-//        {
-//            EventCategory = new EventCategory
-//            {
-//                EventCategoryId = eventCategory.EventCategoryId,
-//                Name = eventCategory.Name
-//            };
-//        }
+        public string Title => EventCategory.EventCategoryId != 0 ? "Edit Event Category" : "New Event Category";
+
     }
 }
