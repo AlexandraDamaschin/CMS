237c655079fdedb3b0f96098f207ba5e8ea33313
kerruish <bryan@kerruish.ie>
2018-02-26 12:15:41 +0000

    update dashboard controller and panel views

--- CMS/Controllers/DashboardController.cs
+++ CMS/Controllers/DashboardController.cs
@@ -6,7 +6,6 @@ namespace CMS.Controllers
 {
     public class DashboardController : Controller
     {
-
         private readonly CmsContext _cms;
 
         public DashboardController()
@@ -23,46 +22,23 @@ namespace CMS.Controllers
             base.Dispose(disposing);
         }
 
-
-
-        // GET: Dashboard
-        public ViewResult Index()
-        {
-            return View();
-        }
-
         public ViewResult DashboardPanels()
         {
-            //            var devicesCount = _cms.Devices.Count();
-            //            if (devicesCount < 20)
-            //            {
-            //                devicesCount = 42;
-            //            }
+            var devicesCount = _cms.Devices.Count();
+            var eventsCount = _cms.Events.Count();
+            var eventCategoriesCount = _cms.EventCategories.Count();
+            var locationsCount = _cms.Locations.Count();
+            var organisersCount = _cms.Organisers.Count();
 
-
-            //            var devicesCount = _cms.Devices.Count();
-            //            var eventCount = _cms.Events.Count();
-            //            var eventCategoriesCount = _cms.EventCategories.Count();
-            //            var locationsCount = _cms.Locations.Count();
-            //            var organisersCount = _cms.Organisers.Count();
-
-            var devicesCount = 999;
-            var eventCount = 999;
-            var eventCategoriesCount = 999;
-            var locationsCount = 999;
-            var organisersCount = 999;
-
-            var viewModel = new DashboardPanelsViewModel(
-            
-                devicesCount, 
-                eventCount,
-                eventCategoriesCount,
-                locationsCount,
-                organisersCount
-            );
-
+            var viewModel = new DashboardPanelsViewModel()
+            {
+                EventsCount = eventsCount,
+                EventCategoriesCount = eventCategoriesCount,
+                LocationsCount = locationsCount,
+                OrganisersCount = organisersCount,
+                DevicesCount = devicesCount
+            };
             return View("_DashboardPanels", viewModel);
         }
-
     }
 }
