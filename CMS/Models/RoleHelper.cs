29fb7784c1e5356cad94e686154b5891a11d99d6
kerruish <bryan@kerruish.ie>
2018-03-04 08:37:00 +0000

    add search view

--- CMS/Models/RoleHelper.cs
+++ CMS/Models/RoleHelper.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace CMS.Models
+﻿namespace CMS.Models
 {
     public static class RoleHelper
     {
---
+++ CMS/ViewModels/EventsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CMS.Models.CMSModel;
+
+namespace CMS.ViewModels
+{
+    public class EventsViewModel
+    {
+
+        public IEnumerable<Event> Events { get; set; }
+
+        public string SearchTerm { get; set; }
+    }
+}
